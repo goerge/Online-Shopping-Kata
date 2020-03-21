f8fee8d1126955d2ec7cb276374fbb88e2185ae8
Görge Albrecht <goerge@code-mentor.com>
2020-03-21 10:21:12 +0100

    reformat test

--- java/src/test/java/codingdojo/OnlineShoppingTest.java
+++ java/src/test/java/codingdojo/OnlineShoppingTest.java
@@ -1,71 +1,67 @@
 package codingdojo;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OnlineShoppingTest {
 
-    private Store backaplan;
-    private Store nordstan;
+  private Store backaplan;
+  private Store nordstan;
 
-    private Item cherryBloom;
-    private Item rosePetal;
-    private Item blusherBrush;
-    private Item eyelashCurler;
-    private Item wildRose;
-    private Item cocoaButter;
-    private Item masterclass;
-    private Item makeoverNordstan;
-    private Item makeoverBackaplan;
+  private Item cherryBloom;
+  private Item rosePetal;
+  private Item blusherBrush;
+  private Item eyelashCurler;
+  private Item wildRose;
+  private Item cocoaButter;
+  private Item masterclass;
+  private Item makeoverNordstan;
+  private Item makeoverBackaplan;
 
-    @BeforeEach
-    public void setUp() {
-        nordstan = new Store("Nordstan", false);
-        backaplan = new Store("Backaplan", true);
+  @BeforeEach
+  public void setUp() {
+    nordstan = new Store("Nordstan", false);
+    backaplan = new Store("Backaplan", true);
 
-        cherryBloom = new Item("Cherry Bloom", "LIPSTICK", 30);
-        rosePetal = new Item("Rose Petal", "LIPSTICK", 30);
-        blusherBrush = new Item("Blusher Brush", "TOOL", 50);
-        eyelashCurler = new Item("Eyelash curler", "TOOL", 100);
-        wildRose = new Item("Wild Rose", "PURFUME", 200);
-        cocoaButter = new Item("Cocoa Butter", "SKIN_CREAM", 250);
+    cherryBloom = new Item("Cherry Bloom", "LIPSTICK", 30);
+    rosePetal = new Item("Rose Petal", "LIPSTICK", 30);
+    blusherBrush = new Item("Blusher Brush", "TOOL", 50);
+    eyelashCurler = new Item("Eyelash curler", "TOOL", 100);
+    wildRose = new Item("Wild Rose", "PURFUME", 200);
+    cocoaButter = new Item("Cocoa Butter", "SKIN_CREAM", 250);
 
-        nordstan.addStockedItems(cherryBloom, rosePetal, blusherBrush, eyelashCurler, wildRose, cocoaButter);
-        backaplan.addStockedItems(cherryBloom, rosePetal, eyelashCurler, wildRose, cocoaButter);
+    nordstan.addStockedItems(cherryBloom, rosePetal, blusherBrush, eyelashCurler, wildRose, cocoaButter);
+    backaplan.addStockedItems(cherryBloom, rosePetal, eyelashCurler, wildRose, cocoaButter);
 
-        // Store events add themselves to the stocked items at their store
-        masterclass = new StoreEvent("Eyeshadow Masterclass", nordstan);
-        makeoverNordstan = new StoreEvent("Makeover", nordstan);
-        makeoverBackaplan = new StoreEvent("Makeover", backaplan);
+    // Store events add themselves to the stocked items at their store
+    masterclass = new StoreEvent("Eyeshadow Masterclass", nordstan);
+    makeoverNordstan = new StoreEvent("Makeover", nordstan);
+    makeoverBackaplan = new StoreEvent("Makeover", backaplan);
+  }
 
+  @Disabled("make this test work")
+  @Test
+  public void switchDeliveryTypeToDrone() {
+    DeliveryInformation deliveryInfo = new DeliveryInformation("HOME_DELIVERY", nordstan, 60);
+    deliveryInfo.setDeliveryAddress("NEARBY");
 
-    }
+    Cart cart = new Cart();
+    cart.addItem(cherryBloom);
+    cart.addItem(blusherBrush);
+    cart.addItem(masterclass);
+    cart.addItem(makeoverNordstan);
 
-    @Disabled("make this test work")
-    @Test
-    public void switchDeliveryTypeToDrone() throws Exception {
-        DeliveryInformation deliveryInfo = new DeliveryInformation("HOME_DELIVERY", nordstan, 60);
-        deliveryInfo.setDeliveryAddress("NEARBY");
+    Session session = new Session(modelObject -> {
+    });
+    session.put("STORE", nordstan);
+    session.put("DELIVERY_INFO", deliveryInfo);
+    session.put("CART", cart);
+    OnlineShopping shopping = new OnlineShopping(session);
 
-        Cart cart = new Cart();
-        cart.addItem(cherryBloom);
-        cart.addItem(blusherBrush);
-        cart.addItem(masterclass);
-        cart.addItem(makeoverNordstan);
-
-        Session session = new Session(modelObject -> {});
-        session.put("STORE", nordstan);
-        session.put("DELIVERY_INFO", deliveryInfo);
-        session.put("CART", cart);
-        OnlineShopping shopping = new OnlineShopping(session);
-
-         shopping.switchStore(backaplan);
-         assertEquals("DRONE", ((DeliveryInformation)session.get("DELIVERY_INFO")).getType());
-    }
-
-
-
+    shopping.switchStore(backaplan);
+    assertEquals("DRONE", ((DeliveryInformation) session.get("DELIVERY_INFO")).getType());
+  }
 }
